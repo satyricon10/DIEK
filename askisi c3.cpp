fb409613a3e84c74c0aeea89dcacc99ee5a71c1e
Chris Ntoukas <chrisntoukas@gmail.com>
2017-11-14 19:29:40 +0200

    Add files via upload

---
+++ askisi c3.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int a,b,c;
+
+printf("Dose 1o vathmo\n");
+printf("Dose 2o vathmo\n");
+printf("Dose 3o vathmo\n");
+scanf("%a","%b","%c",%a,%b,%c);
+
+	if (A,b,c<10)
+	{
+		printf("Dose allo Arithmo\n");		
+		scanf("%d",&A);	
+		if (A<10)
+		{
+		 printf("Arithmos einai ok\n");	
+		}		
+	
+	else 
+	{
+		printf("Arithmos einai OK\n");
+				
+	}
+	}
+	return 0;	
+}
