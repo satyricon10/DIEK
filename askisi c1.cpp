f8c798280ecf8f40aac806273db5e12c3dae30f4
Chris Ntoukas <chrisntoukas@gmail.com>
2017-11-14 19:04:27 +0200

    Add files via upload

---
+++ askisi c1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int Pin=1234;
+printf("Dose Pin\n");
+scanf("%d",&Pin);
+
+	if (Pin==1234)
+	{
+		printf("Pin Correct\n");
+		
+	}	
+	
+	else 
+		{
+		printf("Pin Error\n");		
+	}
+	return 0;	
+}
---
+++ askisi c2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int A;
+
+printf("Dose Arithmo\n");
+scanf("%d",&A);
+
+	if (A>10)
+	{
+		printf("Dose allo Arithmo\n");
+		printf("Dose Arithmo\n");
+		scanf("%d",&A);		
+	}	
+	
+	else 
+		{
+		printf("Arithmos einai OK\n");
+				
+	}
+	return 0;	
+}
