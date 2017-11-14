cdcdf99111ece4d9ca7e18acc98d1cc819077e56
Chris Ntoukas <chrisntoukas@gmail.com>
2017-11-14 18:41:54 +0200

    Add files via upload

---
+++ askisi c.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int A;
+printf("Dose Arithmo\n");
+scanf("%d",&A);
+
+	if (A>0)
+	{
+		printf("Arithmos thetikos\n");
+		
+	}	
+	else if (A<0)
+	{
+		printf("Arithmos arnitikos\n");		
+	}	
+	else 
+		{
+		printf("Arithmos einai miden\n");		
+	}
+	return 0;	
+}
